97e31dc4192abee3a3faf2bc2d031b3b60bb3892
oliverreyes <oliverreyes.a@gmail.com>
2016-08-20 18:19:11 -0700

    Added attribution text and added cc field to countryObj

--- override.js
+++ override.js
@@ -8,12 +8,14 @@ function init(){
 		var bg = document.getElementById('bg-img');
 		var name = document.getElementById('name');
 		var pop = document.getElementById('pop');
+		var att = document.getElementById('att');
 		// set image, cover keeps aspect ratio
 		bg.style.backgroundImage = "url(images/" + response.result.url + ".jpg)";
 		bg.style.backgroundSize = "cover";
 		// country name text font characteristics 
 		name.innerHTML = "<h1>" + response.result.name + "</h1>";
 		pop.innerHTML = "<h3>Population of " + response.result.pop + "</h3>";
+		att.innerHTML = "<a href='" + response.result.link + "'>Photo </a> by " + response.result.author + "/<a href='https://creativecommons.org/licenses/by" + response.result.cc + "/2.0/legalcode'>CC BY</a>";  
 
 		console.log("it passed it");
 	});
